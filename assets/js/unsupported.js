698fc1d3b8421f53888d1eb4e24f4ff917757e3e
mpax235 <168873890+mpax235@users.noreply.github.com>
2025-05-09 15:09:36 -0700

    e

--- assets/js/unsupported.js
+++ assets/js/unsupported.js
@@ -31,6 +31,38 @@ function detectBrowser() {
     const msie = userAgent.indexOf('MSIE ');
     const trident = userAgent.indexOf('Trident/');
 
+    let safarimatch = userAgent.match(/Safari\/(\d+)\./);
+    if (safarimatch) {
+        const continueanyway = document.querySelector('.continue');
+        continueanyway.style.display = 'flex';
+        browser.textContent = 'Safari';
+        browserversion.textContent = 'Safari/' + safarimatch[1] + '.0'; 
+    }
+    
+    let chromiummatch = userAgent.match(/Chrome\/(\d+)\./);
+    if (chromiummatch) {
+        const continueanyway = document.querySelector('.continue');
+        continueanyway.style.display = 'flex';
+        browser.textContent = 'Chromium';
+        browserversion.textContent = 'Chrome/' + chromiummatch[1] + '.0';
+    }
+
+    let ladybirdmatch = userAgent.match(/Ladybird\/(\d+)\./);
+    if (ladybirdmatch) {
+        const continueanyway = document.querySelector('.continue');
+        continueanyway.style.display = 'flex';
+        browser.textContent = 'Ladybird Browser';
+        browserversion.textContent = 'Ladybird/' + ladybirdmatch[1] + '.0';
+    }
+
+    let edgematch = userAgent.match(/Edg\/(\d+)\./);
+    if (edgematch) {
+        const continueanyway = document.querySelector('.continue');
+        continueanyway.style.display = 'flex';
+        browser.textContent = 'Microsoft Edge';
+        browserversion.textContent = 'Edg/' + edgematch[1] + '.0'; 
+    }
+
     let tridentmatch = userAgent.match(/Trident\/(\d+)\./);
     if (msie > -1 || trident > -1) {
         browser.textContent = 'Internet Explorer';
@@ -45,6 +77,14 @@ function detectBrowser() {
         browser.textContent = 'Waterfox Classic / Firefox 68.0 and lower';
         browserversion.textContent = 'Firefox/' + firefoxmatch[1] + '.0';
         return;
+    }
+
+    let firefoxnewmatch = userAgent.match(/Firefox\/(\d+)\./);
+    if (firefoxnewmatch) {
+        const continueanyway = document.querySelector('.continue');
+        continueanyway.style.display = 'flex';
+        browser.textContent = 'Firefox';
+        browserversion.textContent = 'Firefox/' + firefoxmatch[1] + '.0';
     }
 
     let nintendomatch3ds = userAgent.match(/NX\/(\d+)\./);
